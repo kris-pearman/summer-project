43c890d29182550b8b80f7556230277336cc8db2
kris pearman <kris.pearman@outlook.com>
2020-06-08 14:28:46 +0100

    First commit, graphics pipelined, player class created, basic movement added for testing.

---
+++ SummerProject/player.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerProject
+{
+    class player
+    {
+        public int x = 0;
+        public int y = 0;
+        public string spriteName = "Test_graphics/dummy_player";
+            
+        public void GetPlayerInput()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Down))
+                    {
+                y++;
+            };
+            if (state.IsKeyDown(Keys.Up))
+            {
+                y--;
+            };
+            if (state.IsKeyDown(Keys.Left))
+            {
+                x--;
+            };
+            if (state.IsKeyDown(Keys.Right))
+            {
+                x++;
+            };
+        }
+
+    }
+}
